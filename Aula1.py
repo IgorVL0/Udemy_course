8f712f5dcfaddfb28aa71bbe30cdf6aa8561f3dc
Igor <iv.lacerda@hotmail.com>
2020-12-06 10:06:05 -0300

    No commit

--- Aula1.py
+++ Aula1.py
@@ -1,6 +1,7 @@
 any = True
 i = 0
-
+import numpy as np
+"""
 while any:
     i += 1
     if i == 5:
@@ -9,3 +10,7 @@ while any:
         print(f'{i} não é igual a 5 ainda')
 
 print(i)
+"""
+
+for i in np.arange(0,10):
+    print(i)
